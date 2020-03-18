8e6ddd60d94be8af26e6500c9d81e8c80f7cbf11
Jessica Liu <jessica.yzliu@gmail.com>
2020-03-18 15:29:01 -0700

    pinyin audios

--- js/pinyin.js
+++ js/pinyin.js
@@ -1,10 +1,3 @@
-var aAudio = new Audio('sounds/error.mp3');
-var bAudio = new Audio('sounds/error.mp3');
-
 function myAudioFunction(letter) {
-  if (letter == 'a') {
-    aAudio.play();
-  } else if (letter == 'b') {
-    bAudio.play();
-  }
+  new Audio('sounds/' + letter + '.mp3').play();
 }
